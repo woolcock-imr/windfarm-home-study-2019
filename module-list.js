b1fd9e4abe43be0d4d3263bb5c9b47e3e489bfeb
Zhiming Zhou <zzm.vmiis@gmail.com>
2019-10-04 13:44:33 +1000

    update

--- module-list.js
+++ module-list.js
@@ -35,6 +35,7 @@
         "s-sleep-diary-form":	            {url:"$H/m/tasks/sleep-diary-form.html",Table:"91000805",task_name:"sleep-diary"},
 
 
+        //-------------------------------------------------------------------------------------
         "b-actigraphy-data":	            {url:"$H/m/tasks/actigraphy-data.html",Table:"91000806",form_module:"b-actigraphy-form"},
         "b-actigraphy-form":	            {url:"$H/m/tasks/actigraphy-form.html",Table:"91000806",task_name:"actigraphy"},
         "b-sleep-diary-data":	            {url:"$H/m/tasks/sleep-diary-data.html",Table:"91000807",form_module:"b-sleep-diary-form"},
@@ -84,6 +85,56 @@
         "b-blood-and-urine-form":	            {url:"$H/m/tasks/blood-and-urine-form.html",Table:"91000825",task_name:"blood-and-urine"},
         "b-neuro-otological-evaluation-data":	            {url:"$H/m/tasks/neuro-otological-evaluation-data.html",Table:"91000826",form_module:"b-neuro-otological-evaluation-form"},
         "b-neuro-otological-evaluation-form":	            {url:"$H/m/tasks/neuro-otological-evaluation-form.html",Table:"91000826",task_name:"neuro-otological-evaluation"},
+        //-------------------------------------------------------------------------------------
+        "m1-actigraphy-data":	            {url:"$H/m/tasks/actigraphy-data.html",Table:"91000847",form_module:"m1-actigraphy-form"},
+        "m1-actigraphy-form":	            {url:"$H/m/tasks/actigraphy-form.html",Table:"91000847",task_name:"actigraphy"},
+        "m1-sleep-diary-data":	            {url:"$H/m/tasks/sleep-diary-data.html",Table:"91000848",form_module:"m1-sleep-diary-form"},
+        "m1-sleep-diary-form":	            {url:"$H/m/tasks/sleep-diary-form.html",Table:"91000848",task_name:"sleep-diary"},
+        "m1-attitudes-on-wind-farms-data":	{url:"$H/m/tasks/attitudes-on-wind-farms-data.html",Table:"91000849",form_module:"m1-attitudes-on-wind-farms-form"},
+        "m1-attitudes-on-wind-farms-form":	{url:"$H/m/tasks/attitudes-on-wind-farms-form.html",Table:"91000849",task_name:"attitudes-on-wind-farms"},
+        "m1-cardio-and-sleep-illnesses-data":	{url:"$H/m/tasks/cardio-and-sleep-illnesses-data.html",Table:"91000850",form_module:"m1-cardio-and-sleep-illnesses-form"},
+        "m1-cardio-and-sleep-illnesses-form":	{url:"$H/m/tasks/cardio-and-sleep-illnesses-form.html",Table:"91000850",task_name:"cardio-and-sleep-illnesses"},
+        "m1-sleep-disorders-and-patterns-data":	{url:"$H/m/tasks/sleep-disorders-and-patterns-data.html",Table:"91000851",form_module:"m1-sleep-disorders-and-patterns-form"},
+        "m1-sleep-disorders-and-patterns-form":	{url:"$H/m/tasks/sleep-disorders-and-patterns-form.html",Table:"91000851",task_name:"sleep-disorders-and-patterns"},
+        "m1-warwick-edinburgh-mental-well-being-scale-data":	{url:"$H/m/tasks/warwick-edinburgh-mental-well-being-scale-data.html",Table:"91000852",form_module:"m1-warwick-edinburgh-mental-well-being-scale-form"},
+        "m1-warwick-edinburgh-mental-well-being-scale-form":	{url:"$H/m/tasks/warwick-edinburgh-mental-well-being-scale-form.html",Table:"91000852",task_name:"warwick-edinburgh-mental-well-being-scale"},
+        "m1-kessler-10-data":	            {url:"$H/m/tasks/kessler-10-data.html",Table:"91000853",form_module:"m1-kessler-10-form"},
+        "m1-kessler-10-form":	            {url:"$H/m/tasks/kessler-10-form.html",Table:"91000853",task_name:"kessler-10"},
+        "m1-insomnia-severity-index-data":	{url:"$H/m/tasks/insomnia-severity-index-data.html",Table:"91000854",form_module:"m1-insomnia-severity-index-form"},
+        "m1-insomnia-severity-index-form":	{url:"$H/m/tasks/insomnia-severity-index-form.html",Table:"91000854",task_name:"insomnia-severity-index"},
+        
+        "m1-eysenck-data":	{url:"$H/m/tasks/eysenck-data.html",Table:"91000855",form_module:"m1-eysenck-form"},
+        "m1-eysenck-form":	{url:"$H/m/tasks/eysenck-form.html",Table:"91000855",task_name:"eysenck"},
+        "m1-ess-data":	{url:"$H/m/tasks/epworth-sleepiness-scale-data.html",Table:"91000815",form_module:"m1-ess-form"},
+        "m1-ess-form":	{url:"$H/m/tasks/epworth-sleepiness-scale-form.html",Table:"91000815",task_name:"epworth-sleepiness-scale"},
+        "m1-dass21-data":	{url:"$H/m/tasks/depression-anxiety-stress-scales-data.html",Table:"91000857",form_module:"m1-dass21-form"},
+        "m1-dass21-form":	{url:"$H/m/tasks/depression-anxiety-stress-scales-form.html",Table:"91000857",task_name:"depression-anxiety-stress-scales"},
+        "m1-vas-data":	{url:"$H/m/tasks/visual-analogue-scale-data.html",Table:"91000858",form_module:"m1-vas-form"},
+        "m1-vas-form":	{url:"$H/m/tasks/visual-analogue-scale-form.html",Table:"91000858",task_name:"visual-analogue-scale"},
+        //"m1-nas-data":	{url:"$H/m/tasks/noise-annoyance-scale-data.html",Table:"91000818",form_module:"m1-nas-form"},
+        //"m1-nas-form":	{url:"$H/m/tasks/noise-annoyance-scale-form.html",Table:"91000818",task_name:"noise-annoyance-scale"},
+        
+        "m1-tower-of-london-data":	            {url:"$H/m/tasks/tower-of-london-data.html",Table:"91000859",form_module:"m1-tower-of-london-form",quest_module:"m1-tower-of-london-quest"},
+        "m1-tower-of-london-form":	            {url:"$H/m/tasks/tower-of-london-form.html",Table:"91000859"},
+        "m1-tower-of-london-quest":	            {url:"$H/m/tasks/tower-of-london-quest.html",Table:"91000859",task_name:"tower-of-london"},
+        
+        "m1-nback2-data":	            {url:"$H/m/tasks/nback2-data.html",Table:"91000860",form_module:"m1-nback2-form",quest_module:"m1-nback2-quest"},
+        "m1-nback2-form":	            {url:"$H/m/tasks/nback2-form.html",Table:"91000860"},
+        "m1-nback2-quest":	            {url:"$H/m/tasks/nback2-quest.html",Table:"91000860",task_name:"2-back"},
+        
+        "m1-height-weight-data":	            {url:"$H/m/tasks/height-weight-data.html",Table:"91000861",form_module:"m1-height-weight-form"},
+        "m1-height-weight-form":	            {url:"$H/m/tasks/height-weight-form.html",Table:"91000861",task_name:"height-weight"},
+        
+        "m1-blood-pressure-data":	            {url:"$H/m/tasks/blood-pressure-data.html",Table:"91000862",form_module:"m1-blood-pressure-form"},
+        "m1-blood-pressure-form":	            {url:"$H/m/tasks/blood-pressure-form.html",Table:"91000862",task_name:"blood-pressure"},
+        "m1-pwv-data":	            {url:"$H/m/tasks/pwv-data.html",Table:"91000863",form_module:"m1-pwv-form"},
+        "m1-pwv-form":	            {url:"$H/m/tasks/pwv-form.html",Table:"91000863",task_name:"pwv"},
+        "m1-psg-data":	            {url:"$H/m/tasks/psg-300-data.html",Table:"91000864",form_module:"m1-psg-form"},
+        "m1-psg-form":	            {url:"$H/m/tasks/psg-300-form.html",Table:"91000864",task_name:"psg"},
+        "m1-blood-and-urine-data":	            {url:"$H/m/tasks/blood-and-urine-data.html",Table:"91000865",form_module:"m1-blood-and-urine-form"},
+        "m1-blood-and-urine-form":	            {url:"$H/m/tasks/blood-and-urine-form.html",Table:"91000865",task_name:"blood-and-urine"},
+        "m1-neuro-otological-evaluation-data":	            {url:"$H/m/tasks/neuro-otological-evaluation-data.html",Table:"91000866",form_module:"m1-neuro-otological-evaluation-form"},
+        "m1-neuro-otological-evaluation-form":	            {url:"$H/m/tasks/neuro-otological-evaluation-form.html",Table:"91000866",task_name:"neuro-otological-evaluation"},
         //-------------------------------------------------------------------------------------
         //app
         "panel-main":                       {url:"$H/m/panel-main.html",router:1},
---
+++ xcopy/copy.bat
@@ -0,0 +1,2 @@
+xcopy /d /s /y C:\Users\Zhiming\Documents\GitHub\vmiis\projects\sites\114\*.* C:\Users\Zhiming\Documents\GitHub\woolcock-imr\windfarm-home-study-2019\*.*
+pause
